0a7ef2a734deb047df681625c6495ce5c569ebef
lh_intern <tclh123@gmail.com>
2013-12-19 19:04:09 +0800

    add profile::code

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# set -e
+#  
+# here="$(cd "${0%/*}"; pwd)"
+#  
+# for file in "$here"/*; do
+#   name="$(basename "$file")"
+#   [[ $name = bin ]] && dotname="$name" || dotname=".${name}"
+#  
+#   if ! [[ "install readme" =~ $name || $name =~ ".plist" || -e ~/$dotname || -d $file/.git ]]; then
+#     ln -sfhv ${file#$HOME/} "${HOME}/${dotname}"
+#   fi
+# done
+
+
+# ln -sfnv dot-files/dot-vim $HOME/.vim
+
+# ln -sfnv dot-files/dot-vim/vimrc $HOME/.vimrc
