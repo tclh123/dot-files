69487f966d5e32d9f2ae1feb6df5d0b01c2f4ac4
lh_intern <tclh123@gmail.com>
2013-12-19 19:48:53 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/env bash
 
-# set -e
-#  
-# here="$(cd "${0%/*}"; pwd)"
-#  
-# for file in "$here"/*; do
-#   name="$(basename "$file")"
-#   [[ $name = bin ]] && dotname="$name" || dotname=".${name}"
-#  
-#   if ! [[ "install readme" =~ $name || $name =~ ".plist" || -e ~/$dotname || -d $file/.git ]]; then
-#     ln -sfhv ${file#$HOME/} "${HOME}/${dotname}"
-#   fi
-# done
+set -e
 
+PROFILES_DIR=profiles
 
-# ln -sfnv dot-files/dot-vim $HOME/.vim
+here="$(cd "${0%/*}"; pwd)"
+echo "run $0 at $here"
 
-# ln -sfnv dot-files/dot-vim/vimrc $HOME/.vimrc
+throw_error () {
+  echo $1
+  exit 1
+}
+
+main () {
+  folder=$1
+  for file in "$folder"/*; do
+    name="$(basename "$file")"
+    dotname=".${name}"
+    if ! [[ "install readme" =~ $name || -e ~/$dotname || -d $file/.git ]]; then
+      ln -sfnv ${file#$HOME/} "${HOME}/${dotname}"
+    fi
+  done
+}
+
+install_profile () {
+  profile=$1
+  if [ -z "$profile" ]; then
+    throw_error "should choose one profile to install."
+  else
+    echo "install profile $profile"
+    main "$here/$PROFILES_DIR/$profile"
+  fi
+}
+
+install_profile $1
